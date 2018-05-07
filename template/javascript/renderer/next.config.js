fe8ff49dbe2dbc3c6c4ed341f0dc7d25c2705333
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-05-08 01:24:02 +0900

    Remove unnecessary export settings

--- template/javascript/renderer/next.config.js
+++ template/javascript/renderer/next.config.js
@@ -1,7 +1,3 @@
 exports.webpack = config => Object.assign(config, {
   target: 'electron-renderer'
 })
-
-exports.exportPathMap = () => ({
-  '/home': { page: '/home' }
-})
--- template/typescript/renderer/next.config.js
+++ template/typescript/renderer/next.config.js
@@ -4,7 +4,3 @@ module.exports = withTypeScript()
 exports.webpack = config => Object.assign(config, {
   target: 'electron-renderer'
 })
-
-exports.exportPathMap = () => ({
-  '/home': { page: '/home' }
-})
