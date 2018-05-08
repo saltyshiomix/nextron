c65f0614333cc0a2d241bdad203b640d7314f849
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-05-08 22:29:41 +0900

    Fix typo...

--- template/typescript/renderer/next.config.js
+++ template/typescript/renderer/next.config.js
@@ -1,7 +1,7 @@
 const withTypescript = require('@zeit/next-typescript')
 module.exports = withTypescript({
   webpack: (config, options) => {
-    confit.target = 'electron-renderer'
+    config.target = 'electron-renderer'
     return config
   }
 })
