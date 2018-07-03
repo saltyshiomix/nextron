5b8cefb72f684dd28e18d46437a1bb75b05f605d
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-03 21:11:34 +0900

    Resolve exported absolute paths to relative ones

--- src/lib/next/build.ts
+++ src/lib/next/build.ts
@@ -1,12 +1,20 @@
 import { execSync } from 'child_process'
 import { join, sep } from 'path'
-import { copy, remove } from 'fs-extra'
+import { copy, remove, readFileSync, writeFileSync } from 'fs-extra'
+import * as fg from 'fast-glob'
+import resolveExportedPaths from './resolve-exported-paths'
 
 export default async function build(rendererDir: string): Promise<void> {
   const cwd: string = process.cwd()
   const outdir: string = join(cwd, rendererDir, 'out')
+  const appdir: string = join(cwd, 'app')
   execSync(`node_modules${sep}.bin${sep}next build ${rendererDir}`, { cwd })
   execSync(`node_modules${sep}.bin${sep}next export ${rendererDir}`, { cwd })
-  await copy(outdir, join(cwd, 'app'))
+  await copy(outdir, appdir)
   await remove(outdir)
+
+  const pages: string[] = fg.sync(join(appdir, '**/*.html'))
+  pages.forEach(page => {
+  	writeFileSync(page, resolveExportedPaths(readFileSync(page).toString()))
+  })
 }
