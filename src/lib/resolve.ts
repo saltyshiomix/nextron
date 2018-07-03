f5e39e3294ea8600af806c964f6bb309a7edc7d7
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-03 20:49:28 +0900

    Fix nextron.resolve() function when development mode

--- src/lib/resolve.ts
+++ src/lib/resolve.ts
@@ -1,10 +1,8 @@
 import { sep } from 'path'
 
-const isProd: boolean = process.env.NODE_ENV === 'production'
-
 export default function resolve(pathname: string): string {
   if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(pathname)) {
-    return isProd ? `..${sep}${pathname}` : `/${pathname}`
+    return `..${sep}${pathname}`
   }
-  return isProd ? `..${sep}${pathname}${sep}index.html` : `/${pathname}`
+  return `..${sep}${pathname}${sep}index.html`
 }
