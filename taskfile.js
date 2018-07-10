36534531849b47b3fb66eb750ddb18dafaa06a52
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-11 03:19:29 +0900

    Fix line breaks bug #2
    dos2unix
    mac2unix
    
    It fixes `env: node\r: No such file or directory` error

--- taskfile.js
+++ taskfile.js
@@ -1,11 +1,40 @@
+const { resolve } = require('path')
+const { execSync } = require('child_process')
+const fg = require('fast-glob')
+
+function convToUnixFormat() {
+  const isWindows = /^win/.test(process.platform)
+  if (!isWindows) {
+    return
+  }
+
+  const cwd = process.cwd()
+  const files = fg.sync('dist/**/*', { cwd })
+  files.forEach(function(file) {
+    execSync(`.\\bin\\dos2unix.exe ${file}`, { cwd })
+  })
+}
+
 module.exports = {
+  *cleanDist(task) {
+    yield task.clear('dist')
+  },
+  *cleanWorkspace(task) {
+    yield task.clear('workspace')
+  },
   *tsc(task) {
     yield task.source('src/**/*.ts').typescript().target('dist', { mode: '0755' })
   },
+  *toUnixFormat(task) {
+    convToUnixFormat()
+  },
+  *chmod755(task) {
+    yield task.target('dist/cli/nextron*.js', { mode: '0755' })
+  },
   *build(task) {
-    yield task.clear('dist').clear('workspace').start('tsc')
+    yield task.serial(['cleanDist', 'cleanWorkspace', 'tsc', 'toUnixFormat', 'chmod755'])
   },
   *release(task) {
-    yield task.clear('dist').start('tsc')
+    yield task.serial(['cleanDist', 'tsc', 'toUnixFormat', 'chmod755'])
   }
 }
