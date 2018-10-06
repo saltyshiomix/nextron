257dfe9cc2df0c2669d44a1d4ca676d05914052e
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-10-07 07:29:52 +0900

    Refactoring: simplify

--- src/lib/util/resolve.ts
+++ src/lib/util/resolve.ts
@@ -1,11 +1,9 @@
-import { join } from 'path'
-
 export default function resolve(pathname: string): string {
   if (process.env.NODE_ENV === 'production') {
     if (/\.(png|jpe?g|gif|svg|js|css)(\?.*)?$/.test(pathname)) {
-      return join(`../${pathname}`)
+      return `../${pathname}`
     }
-    return join(`../${pathname}/index.html`)
+    return `../${pathname}/index.html`
   }
   return '/' + pathname
 }
