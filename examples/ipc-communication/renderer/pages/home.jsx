2b7cbf14b0942265b9cf1570a280459f2ff217af
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-11-07 14:35:41 +0900

    Add more example codes to `examples/ipc-communication`

--- examples/ipc-communication/renderer/pages/home.jsx
+++ examples/ipc-communication/renderer/pages/home.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import electron from 'electron'
 import { resolveWithIpc, LinkWithIpc } from '../helpers'
 
 export default class extends React.Component {
+  // prevent SSR webpacking
+  ipcRenderer = electron.ipcRenderer || false
+
   state = {
-    logo: null
+    logo: null,
+    ipcResult: 'no ipc messaging'
   }
 
   componentDidMount() {
     this.setState({
       logo: resolveWithIpc('static/logo.png')
     })
+  }
+
+  onClickWithIpc = () => {
+    if (this.ipcRenderer) {
+      this.ipcRenderer.send('ping-pong', 'some data from ipcRenderer')
+      this.ipcRenderer.on('ping-pong', (event, data) => {
+        this.setState({
+          ipcResult: data
+        })
+      })
+    }
+  }
+
+  onClickWithIpcSync = () => {
+    if (this.ipcRenderer) {
+      this.setState({
+        ipcResult: this.ipcRenderer.sendSync('ping-pong-sync', 'some data from ipcRenderer')
+      })
+    }
   }
 
   render() {
@@ -19,6 +43,10 @@ export default class extends React.Component {
           ⚡ Electron + Next.js ⚡ - <LinkWithIpc href="next"><a>To Next Page</a></LinkWithIpc>
         </p>
         <img src={this.state.logo} />
+        <hr />
+        <button onClick={this.onClickWithIpc}>IPC messaging</button>
+        <button onClick={this.onClickWithIpcSync}>IPC messaging (sync)</button>
+        <p>{this.state.ipcResult}</p>
       </div>
     )
   }
