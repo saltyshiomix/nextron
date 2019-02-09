3b9d5815aceae4a1048f0c3ffdb206fb41f0f662
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-02-09 18:30:19 +0900

    Refactoring: rename(enableHotReload => exitOnChange) #19

--- examples/_template/js/main/helpers/exit-on-change.js
+++ examples/_template/js/main/helpers/exit-on-change.js
@@ -2,7 +2,7 @@ import { watchFile } from 'fs'
 import { join } from 'path'
 import { app } from 'electron'
 
-export default function enableHotReload() {
+export default function exitOnChange() {
   watchFile(join(process.cwd(), 'app/background.js'), () => {
     app.exit(0)
   })
--- examples/_template/ts/main/helpers/exit-on-change.ts
+++ examples/_template/ts/main/helpers/exit-on-change.ts
@@ -2,7 +2,7 @@ import { watchFile } from 'fs'
 import { join } from 'path'
 import { app } from 'electron'
 
-export default function enableHotReload(): void {
+export default function exitOnChange(): void {
   watchFile(join(process.cwd(), 'app/background.js'), () => {
     app.exit(0)
   })
