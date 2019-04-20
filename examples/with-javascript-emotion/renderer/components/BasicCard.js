20d0e0c5058e058ca96110dda8b5e2e49d119696
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-04-20 17:13:45 +0900

    Improve design of emotion examples

--- examples/with-javascript-emotion/renderer/components/BasicCard.js
+++ examples/with-javascript-emotion/renderer/components/BasicCard.js
@@ -9,7 +9,7 @@ border-right: none;
 border-bottom: none;
 box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
 transition: all 0.1s linear;
-margin: 3rem 0;
+margin: 2rem 0;
 padding: 1rem 0.5rem;
 `;
 
