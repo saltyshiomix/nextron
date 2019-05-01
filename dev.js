5611e64f99abd5eb237e89f4475a22b41eb3a761
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-02 04:46:57 +0900

    Refactoring: rename

--- dev.js
+++ dev.js
@@ -4,19 +4,19 @@ const { execSync } = require('child_process');
 const chalk = require('chalk');
 
 async function dev() {
-  let template = 'with-javascript-emotion';
+  let example = 'with-javascript-emotion';
   if (3 <= process.argv.length) {
-    const newTemplate = process.argv[2];
-    if (!existsSync(resolve(__dirname, `examples/${newTemplate}`))) {
-      console.log(chalk.red(`Not found examples/${newTemplate}`));
+    const newExample = process.argv[2];
+    if (!existsSync(resolve(__dirname, `examples/${newExample}`))) {
+      console.log(chalk.red(`Not found examples/${newExample}`));
       console.log('');
       process.exit(1);
     }
-    template = newTemplate;
+    example = newExample;
   }
 
   await remove('workspace');
-  execSync('node ' + resolve(__dirname, `bin/nextron init workspace --example ${template}`), {
+  execSync('node ' + resolve(__dirname, `bin/nextron init workspace --example ${example}`), {
     cwd: __dirname,
     stdio: 'inherit',
   });
