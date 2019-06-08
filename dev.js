dbc5e6eaedabad91aafcaebba27ee8831030b5f7
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2019-06-08 21:09:10 +0530

    use yalc for linking in dev environment

--- dev.js
+++ dev.js
@@ -2,9 +2,12 @@ const { remove, existsSync } = require('fs-extra');
 const { resolve } = require('path');
 const { execSync } = require('child_process');
 const chalk = require('chalk');
+const { npxSync } = require('node-npx');
+
+const stdio = 'inherit';
 
 async function dev() {
-  let example = 'with-javascript';
+  let example = 'with-typescript';
   if (3 <= process.argv.length) {
     const newExample = process.argv[2];
     if (!existsSync(resolve(__dirname, `examples/${newExample}`))) {
@@ -18,18 +21,25 @@ async function dev() {
   await remove('workspace');
   execSync('node ' + resolve(__dirname, `bin/nextron init workspace --example ${example}`), {
     cwd: __dirname,
-    stdio: 'inherit',
+    stdio,
   });
+
+  npxSync('yalc', ['publish'], { stdio });
+
   execSync('cd workspace');
-  execSync('yarn link nextron && yarn', {
+  npxSync('yalc', ['add', 'nextron'], {
     cwd: resolve(__dirname, 'workspace'),
-    stdio: 'inherit',
+    stdio,
+  });
+  execSync('yarn', {
+    cwd: resolve(__dirname, 'workspace'),
+    stdio,
   });
 
   execSync('yarn dev', {
     cwd: resolve(__dirname, 'workspace'),
-    stdio: 'inherit',
-  })
+    stdio,
+  });
 }
 
 dev();
