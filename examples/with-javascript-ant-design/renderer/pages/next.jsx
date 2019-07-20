597667330931de3a19a017ea98ba100e35b541d7
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-07-21 03:03:16 +0900

    Add a next page to examples/with-javascript-ant-design #40

---
+++ examples/with-javascript-ant-design/renderer/pages/next.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Head from 'next/Head';
+import Link from 'next/Link';
+import {
+  Layout,
+  Result,
+} from 'antd';
+
+import 'antd/dist/antd.css';
+
+const {
+  Header,
+  Content,
+} = Layout;
+
+export default () => (
+  <React.Fragment>
+    <Head>
+      <title>Next - Nextron (with-javascript-ant-design)</title>
+    </Head>
+
+    <Header>
+      <Link href="/home">
+        <a>Go to home page</a>
+      </Link>
+    </Header>
+    <Content style={{ padding: 48 }}>
+      <Result
+        status="success"
+        title="Nextron"
+        subTitle="with Ant Design"
+      />
+    </Content>
+  </React.Fragment>
+);
