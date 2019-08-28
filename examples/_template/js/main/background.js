2cad7cdc4346bf0836c1310e3b373ed0aa32977c
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-08-28 13:57:39 +0900

    Fix homeUrl when production

--- examples/_template/js/main/background.js
+++ examples/_template/js/main/background.js
@@ -21,7 +21,7 @@ if (isProd) {
     height: 600,
   });
 
-  const homeUrl = isProd ? 'app://./home' : 'http://localhost:8888/home';
+  const homeUrl = isProd ? 'app://./home.html' : 'http://localhost:8888/home';
   mainWindow.loadURL(homeUrl);
 
   if (!isProd) {
