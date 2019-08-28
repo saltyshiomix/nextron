26312f9071dd7a44452c8ff86ceb9e09ac9c787f
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-08-28 13:04:43 +0900

    Remove unnecessary files from helpers

--- examples/_template/ts/main/helpers/index.ts
+++ examples/_template/ts/main/helpers/index.ts
@@ -1,5 +1,7 @@
 import createWindow from './create-window';
 import exitOnChange from './exit-on-change';
-import ipc from './ipc';
 
-export { createWindow, exitOnChange, ipc };
+export {
+  createWindow,
+  exitOnChange,
+};
