5d5572ddd2a00be3592d2a9cdc608509cadcd349
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-09-02 09:30:47 +0900

    Support pnpm (nextron commands) #48

--- dev.js
+++ dev.js
@@ -3,10 +3,41 @@ const { resolve } = require('path');
 const { execSync } = require('child_process');
 const chalk = require('chalk');
 
+async function detectPackageManager() {
+  const { promisify } = require('util');
+  const { exec: defaultExec } = require('child_process');
+  const cwd = process.cwd();
+  const exec = promisify(defaultExec);
+
+  let pm = 'yarn';
+  try {
+    await exec(`${pm} -v`, { cwd });
+  } catch (_) {
+    pm = 'pnpm';
+    try {
+      await exec(`${pm} -v`, { cwd });
+    } catch (_ignore) {
+      pm = 'npm';
+      try {
+        await exec(`${pm} -v`, { cwd });
+      } catch (_) {
+        pm = null;
+      }
+    }
+  }
+
+  if (pm === null) {
+    console.log(chalk.red('No available package manager! (`yarn`, `pnpm` or `npm` is needed)'));
+    process.exit(1);
+  }
+
+  return pm;
+}
+
 async function dev() {
   process.env.NODE_ENV = 'testing';
 
-  let example = 'with-javascript-emotion';
+  let example = 'with-javascript';
   if (3 <= process.argv.length) {
     const newExample = process.argv[2];
     if (!existsSync(resolve(__dirname, `examples/${newExample}`))) {
@@ -24,7 +55,8 @@ async function dev() {
     stdio: 'inherit',
   });
 
-  execSync('yarn && yarn dev', {
+  const pm = await detectPackageManager();
+  execSync(`${pm} install && ${pm} run dev`, {
     cwd: resolve(__dirname, 'workspace'),
     stdio: 'inherit',
   });
