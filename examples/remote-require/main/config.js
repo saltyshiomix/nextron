00ab952a372b2cdb5daf4cfaa516ac82e76686c8
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2021-01-16 15:52:41 +0900

    Remove "remote-require" example #146 big thanks to @kevireilly

--- examples/remote-require/main/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  message: 'This message is from the main process of `config.js`',
-};
-
-export default config;
--- examples/remote-require/nextron.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  webpack: (defaultConfig, env) => Object.assign(defaultConfig, {
-    entry: {
-      background: './main/background.js',
-      config: './main/config.js',
-    },
-  }),
-};
