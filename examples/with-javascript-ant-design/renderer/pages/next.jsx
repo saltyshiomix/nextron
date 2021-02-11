26279fa4d0c58a5de19b26c25f5c0c5cefcdc9db
Youngsuk Chung <yschung@nearthlab.com>
2021-02-12 01:36:57 +0900

    Fixed next/head, next/link bug.
    Changed followings:
    - next/Head -> next/head
    - next/Link -> next/link
    
    I faced error messaged during building example.
    
    ModuleNotFoundError: Module not found: Error: Can't resolve 'next/Head' in '/home/yschung/workspace/mav-nextron/renderer/pages'
    
    Official nextjs document uses 'next/head', not 'next/Head'
    (https://nextjs.org/docs/api-reference/next/head)
    
    Tested on
    - next: 10.0.6,

--- examples/with-javascript-ant-design/renderer/pages/next.jsx
+++ examples/with-javascript-ant-design/renderer/pages/next.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Head from 'next/Head';
-import Link from 'next/Link';
+import Head from 'next/head';
+import Link from 'next/link';
 import {
   Layout,
   Result,
