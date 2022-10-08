34ce1f849387be5a9f836ddef5fc96c673b5bf19
phibersoft <spirnet@hotmail.com>
2022-10-08 08:57:10 +0300

    Migrated to Material UI v5 from v4

--- examples/with-typescript-material-ui/renderer/lib/theme.ts
+++ examples/with-typescript-material-ui/renderer/lib/theme.ts
@@ -1,19 +1,19 @@
-import { createTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
+import {createTheme} from '@mui/material/styles';
+import {red} from '@mui/material/colors';
 
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+// Create a theme instance.
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
 });
+
+export default theme;
--- examples/with-typescript-material-ui/renderer/pages/next.tsx
+++ examples/with-typescript-material-ui/renderer/pages/next.tsx
@@ -1,43 +1,41 @@
 import React from 'react';
 import Head from 'next/head';
-import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import Link from '../components/Link';
+import {styled} from '@mui/material';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      textAlign: 'center',
-      paddingTop: theme.spacing(4),
-    },
-  })
-);
+const Root = styled('div')(({theme}) => {
+    return {
+        textAlign: 'center',
+        paddingTop: theme.spacing(4),
+    };
+});
+
 
 function Next() {
-  const classes = useStyles({});
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Next - Nextron (with-typescript-material-ui)</title>
-      </Head>
-      <div className={classes.root}>
-        <Typography variant="h4" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          with Nextron
-        </Typography>
-        <Typography gutterBottom>
-          <Link href="/home">Go to the home page</Link>
-        </Typography>
-        <Button variant="contained" color="primary">
-          Do nothing button
-        </Button>
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Next - Nextron (with-typescript-material-ui)</title>
+            </Head>
+            <Root>
+                <Typography variant="h4" gutterBottom>
+                    Material-UI
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                    with Nextron
+                </Typography>
+                <Typography gutterBottom>
+                    <Link href="/home">Go to the home page</Link>
+                </Typography>
+                <Button variant="contained" color="primary">
+                    Do nothing button
+                </Button>
+            </Root>
+        </React.Fragment>
+    );
 };
 
 export default Next;
