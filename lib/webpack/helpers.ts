504662239f3beec0837fe44882e01320a5da45d9
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2023-01-08 15:08:21 +0900

    fix: require webpack-merge

--- lib/webpack/helpers.ts
+++ lib/webpack/helpers.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import merge from 'webpack-merge';
+import { merge } from 'webpack-merge';
 import configure from './webpack.config';
 
 const existsSync = (f: string): boolean => {
