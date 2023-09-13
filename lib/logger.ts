c27a8d2d31a89994ac0f0fdd4376eeacbe2db73f
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2023-09-14 02:12:24 +0900

    fix: Apply linter and formatter

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,5 +1,5 @@
-import chalk from 'chalk';
+import chalk from 'chalk'
 
 export default function log(text: string) {
-  console.log(chalk`{cyan [nextron]} ${text}`);
+  console.log(chalk`{cyan [nextron]} ${text}`)
 }
