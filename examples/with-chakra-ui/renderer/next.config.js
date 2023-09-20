8f94b36e3ad279e6446b2345786f63c3409125be
Ketan Patel <ktan.p.patel@gmail.com>
2023-09-20 20:51:36 +0530

    fix: update next.config.js - for production build

--- examples/with-chakra-ui/renderer/next.config.js
+++ examples/with-chakra-ui/renderer/next.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  trailingSlash: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = 'electron-renderer'
     }
-
     return config
+  },
+  images: {
+    unoptimized: true,
   },
 }
