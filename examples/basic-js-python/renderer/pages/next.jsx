949b50db9286a4cd70268347e56cf6faa63842c0
Bayangmbe Mounmo <bayangp0@gmail.com>
2023-12-07 01:23:07 +0100

    added scripts

--- examples/basic-js-python/renderer/pages/next.jsx
+++ examples/basic-js-python/renderer/pages/next.jsx
@@ -6,7 +6,7 @@ export default function NextPage() {
   return (
     <React.Fragment>
       <Head>
-        <title>Next - Nextron (basic-lang-javascript)</title>
+        <title>Next - Nextron (basic-js-python)</title>
       </Head>
       <div>
         <p>
