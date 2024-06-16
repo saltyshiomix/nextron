798feeb7ddaae255fcd7aed840ca3aebc6c66c9f
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2024-06-16 15:12:18 +0900

    hotfix: Correct next distDir

--- examples/basic-lang-javascript-python/renderer/next.config.js
+++ examples/basic-lang-javascript-python/renderer/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'export',
-  distDir: '../app',
+  distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
   trailingSlash: true,
   images: {
     unoptimized: true,
